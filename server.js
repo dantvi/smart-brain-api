9d5a3291d9f12f92e300ea760e7f97471794f3da
dantvi <daniel.tving@gmail.com>
2024-10-19 17:20:07 +0200

    fix indentation

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require('./db-config');
 
 
 db.select('*').from('users').then(data => {
-    console.log(data);
+  console.log(data);
 });
 
 const app = express();
@@ -15,82 +15,82 @@ app.use(cors());
 app.use(express.json());
 
 const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
 }
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+  res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error loggin in');
-    }
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json('error loggin in');
+  }
 });
 
 app.post('/register', (req, res) => {
-    const { name, email } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            name: name,
-            email: email,
-            joined: new Date()
-        })
-        .then(user => {
-            res.json(user[0]);
-        })
-        .catch(err => res.status(400).json('unable to register'));
+  const { name, email } = req.body;
+  db('users')
+    .returning('*')
+    .insert({
+      name: name,
+      email: email,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users')
-        .where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('not found');
-            }
-        })
-        .catch(err => res.status(400).json('error getting user'));
+  const { id } = req.params;
+  db.select('*').from('users')
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'));
 });
 
 app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(400).json('user not found');
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
     }
+  });
+  if (!found) {
+    res.status(400).json('user not found');
+  }
 });
 
 app.listen(3000, () => {
-    console.log('app is running on port 3000');
+  console.log('app is running on port 3000');
 });
